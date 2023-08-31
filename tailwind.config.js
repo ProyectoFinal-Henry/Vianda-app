a46f25f3b3cedd06555216964ef338608bb6d20d
davidalejoms <davidalejoms@gmail.com>
2023-08-31 14:33:39 -0500

    'deisy first'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      {
+        viandapp: {
+          primary: "#FFE26C",
+
+          secondary: "#FFF369",
+
+          accent: "#7CC964",
+
+          neutral: "#383838",
+
+          "base-100": "#F3FAED",
+
+          info: "#a3e635",
+
+          success: "#36d399",
+
+          warning: "#F76353",
+
+          error: "#F76353",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
