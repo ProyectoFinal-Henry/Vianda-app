1f34b0dce3d7cf2b8709568007cdf528f8d59f9d
mauro fallico <mauro.fallico@Barracas.com.ar>
2023-08-31 13:03:20 -0300

    se agregaron las peticiones GET y POST para Viandas

--- src/app/api/viandas/route.js
+++ src/app/api/viandas/route.js
@@ -4,13 +4,19 @@ import { NextResponse } from 'next/server';
 const prisma = new PrismaClient()
 
 export async function GET(){
-    const viandas = await prisma.Vianda.findMany()
-    return NextResponse.json(viandas);
+    try {
+        const viandas = await prisma.Vianda.findMany()
+        return NextResponse.json(viandas);
+    } catch (error) {
+        return NextResponse.json('Error al obtener las viandas')
+    }
+    
 }
 
 export async function POST(request){
-    const { nombre, tipo, descripcion, ingredientes, imagen, stock } = await request.json()
-    await prisma.Vianda.create({
+    try {
+        const { nombre, tipo, descripcion, ingredientes, imagen, stock } = await request.json()
+        await prisma.Vianda.create({
         data: {
             nombre,
             tipo,
@@ -20,5 +26,9 @@ export async function POST(request){
             stock
         }
     })
-    return NextResponse.json("Vianda creada exitosamente!")
+        return NextResponse.json("Vianda creada exitosamente!")
+    } catch (error) {
+        return NextResponse.json("No se pudo crear la vianda")
+    }
+    
 }
