35c1293deaed1bc9630a9ff326a23efc4f5ccc84
mauro <maurofallico@gmail.com>
2023-09-01 12:20:43 -0300

    peticion DELETE (borrado logico) para Vianda y Carta

---
+++ src/app/api/carta/[cartaId]/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import { prisma } from '../../../../libs/prisma'
+
+export async function DELETE(request, { params }) {
+  try {
+    // Se busca la carta a traves de params
+    const carta = await prisma.Carta.findUnique({
+      where: { id: Number(params.cartaId) },
+      select: { status: true },
+    });
+
+    // Se evalua si existe la carta
+    if (!carta) {
+      return NextResponse.json("Carta no encontrada", { status: 404 });
+    }
+
+    // Se invierte el valor booleano {status}
+    await prisma.Carta.update({
+      where: { id: Number(params.cartaId) },
+      data: { status: !carta.status },
+    });
+
+    return NextResponse.json("Carta modificada correctamente");
+  } catch (error) {
+    return NextResponse.json("Error al intentar modificar la carta", { status: 403 });
+  }
+}
