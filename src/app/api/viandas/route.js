86cef8826a4fdccf3211d12ddfe9a00d189912fd
FranMeza <ing.meza.francisco@gmail.com>
2023-09-01 09:43:53 -0500

    importe de prisma client desde una unica instancia

--- src/app/api/viandas/route.js
+++ src/app/api/viandas/route.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/libs/prisma";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function GET() {
   try {
