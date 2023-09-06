5674c16b3a719f3b1b52c433d7eef76597e1fd79
mauro <maurofallico@gmail.com>
2023-09-06 01:20:07 -0300

    Filtros mejorados (combinables con la SearchBar)

--- src/components/adminLayout/SearchBarViandas.jsx
+++ src/components/adminLayout/SearchBarViandas.jsx
@@ -1,35 +1,46 @@
 "use client"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { useState } from "react"
-import { FaSearch } from "react-icons/fa"
-export const dynamic = "force-dynamic"
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
 const SearchBarViandas = () => {
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
+  const searchParams = useSearchParams();
 
-  const pathName = usePathname()
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
-  const urltogo = `${pathName}?search=${searchValue}`
+  const buildQueryString = () => {
+    const params = new URLSearchParams(searchParams);
+    if (searchValue.trim() === "") {
+      params.delete("search");
+    } else {
+      params.set("search", searchValue);
+    }
+    const queryString = params.toString();
+    return queryString;
+  };
+
   return (
-    <>
-      <div className="flex items-center gap-x-0 min-w-[40%] md:min-w-[35%] ">
-        <input
-          onChange={(e) => setSearchValue(e.target.value)}
-          type="text"
-          placeholder="Haburguesa, Pasta etc.."
-          className="input input-bordered input-md w-full max-w-xl"
-        />
-        <Link
-          href={urltogo}
-          className="-ml-10"
-        >
-          <button className=" btn btn-warning text-white ">
+    <div className="flex items-center gap-x-0 min-w-[40%] md:min-w-[35%]">
+      <input
+        onChange={handleSearchChange}
+        type="text"
+        placeholder="Hamburguesa, Pasta, etc..."
+        className="input input-bordered input-md w-full max-w-xl"
+        value={searchValue}
+      />
+      <Link href={{ search: buildQueryString() }}
+        className="-ml-10">
+          <button className="btn btn-warning text-white">
             <FaSearch />
           </button>
-        </Link>
-      </div>
-    </>
-  )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default SearchBarViandas
+export default SearchBarViandas;
