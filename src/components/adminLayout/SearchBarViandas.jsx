3f2b715f181cb71099b2d4563fc31dbaaa0c6277
mauro <maurofallico@gmail.com>
2023-09-06 01:38:13 -0300

    Filtros mejorados (combinables con la SearchBar)

--- src/components/adminLayout/SearchBarViandas.jsx
+++ src/components/adminLayout/SearchBarViandas.jsx
@@ -1,16 +1,15 @@
 "use client"
-
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useState } from "react";
-import { FaSearch } from "react-icons/fa";
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { FaSearch } from "react-icons/fa"
+export const dynamic = "force-dynamic"
 
 const SearchBarViandas = () => {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("")
   const searchParams = useSearchParams();
-
   const handleSearchChange = (e) => {
-    setSearchValue(e.target.value);
+  setSearchValue(e.target.value);
   };
 
   const buildQueryString = () => {
@@ -25,22 +24,26 @@ const SearchBarViandas = () => {
   };
 
   return (
-    <div className="flex items-center gap-x-0 min-w-[40%] md:min-w-[35%]">
-      <input
-        onChange={handleSearchChange}
-        type="text"
-        placeholder="Hamburguesa, Pasta, etc..."
-        className="input input-bordered input-md w-full max-w-xl"
-        value={searchValue}
-      />
-      <Link href={{ search: buildQueryString() }}
-        className="-ml-10">
-          <button className="btn btn-warning text-white">
+    <>
+      <div className="flex items-center gap-x-0 min-w-[40%] md:min-w-[35%] ">
+        <input
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="Hamburguesa, Pasta, etc..."
+          className="input input-bordered input-md w-full max-w-xl"
+          value={searchValue}
+        />
+        <Link
+          href={{ search: buildQueryString() }}
+          className="-ml-10"
+        >
+          <button className=" btn btn-warning text-white ">
             <FaSearch />
           </button>
-      </Link>
-    </div>
-  );
-};
+        </Link>
+      </div>
+    </>
+  )
+}
 
-export default SearchBarViandas;
+export default SearchBarViandas
