d0f6932e5144c86b23f9a5d8f15cb54b094a343e
davidalejoms <davidalejoms@gmail.com>
2023-09-07 10:35:08 -0500

    'rutas para antonella'

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,26 +1,34 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link"
+import Image from "next/image"
 
 function NavBar() {
   return (
-    <nav className= "bg-primary">
-      
-        <div className="container mx-auto flex justify-between items-center px-3">
-          <Link href="/">
-            <Image width={150} height={100} src="/verde.png" alt="imagen logo" />
-          </Link>
-            <div className="form-control" >
-                  <input type="text" placeholder="Buscar" className=" input input-bordered-none" />
-                </div>
-                <ul className='flex text-lg font-bold'>
-                <li className='btn btn-primary font-bold'>
-                  <Link href="">Registrarse</Link>
-                </li>
-                <li className='btn btn-primary font-bold'>
-                  <Link href="/catalog/home">Ingresar</Link>
-                </li>
-            </ul>
+    <nav className="bg-primary">
+      <div className="container mx-auto flex justify-between items-center px-3">
+        <Link href="/">
+          <Image
+            width={150}
+            height={100}
+            src="/verde.png"
+            alt="imagen logo"
+          />
+        </Link>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Buscar"
+            className=" input input-bordered-none"
+          />
         </div>
+        <ul className="flex text-lg font-bold">
+          <li className="btn btn-primary font-bold">
+            <Link href="/catalog/registro">Registrarse</Link>
+          </li>
+          <li className="btn btn-primary font-bold">
+            <Link href="/catalog/login">Ingresar</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
