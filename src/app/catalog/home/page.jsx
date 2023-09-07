07e44251c66bd4d15f09ace9df721ce2ea69de3c
FranMeza <ing.meza.francisco@gmail.com>
2023-09-07 09:12:53 -0500

    error en el nombre de un componente

--- src/app/catalog/home/page.jsx
+++ src/app/catalog/home/page.jsx
@@ -1,35 +1,43 @@
-import CardTipes from "@/components/cards/CardTipes"
-import cardRecomend from "@/components/cards/cardRecomend"
-import Pagination from "@/components/cards/Pagination"
-import axios from "axios"
+import CardTipes from "@/components/cards/CardTipes";
+import CardRecomend from "@/components/cards/cardRecomend";
+import Pagination from "@/components/cards/Pagination";
+import axios from "axios";
 
 async function Homepage() {
-  const res = await axios.get(`${process.env.LOCALHOST}/api/viandas`)
-  const data = res.data
+  const res = await axios.get(`${process.env.LOCALHOST}/api/viandas`);
+  const data = res.data;
 
   return (
     <>
       <div className="my-10">
         <div className="flex justify-center sm:flex sm:justify-end sm:mr-32">
-          <button className="btn btn-info hover:bg-lime-300 font-bold text-lg">INICIAR PEDIDO</button>
+          <button className="btn btn-info hover:bg-lime-300 font-bold text-lg">
+            INICIAR PEDIDO
+          </button>
         </div>
 
         <div className="flex justify-center">
-          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">Categorias en nuestro Menu</h1>
+          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">
+            Categorias en nuestro Menu
+          </h1>
         </div>
         <div className="grid grid-rows-4 gap-3 sm:flex justify-evenly flex-wrap mt-6">
           <CardTipes />
         </div>
 
         <div className="flex justify-center">
-          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">Recomendados de la semana</h1>
+          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">
+            Recomendados de la semana
+          </h1>
         </div>
         <div className="grid grid-rows-3 gap-3 sm:flex justify-evenly flex-wrap mt-6">
           <CardRecomend />
         </div>
 
         <div className="flex justify-center">
-          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">Menu</h1>
+          <h1 className="mt-10 p-5 badge badge-secondary text-lg font-bold">
+            Menu
+          </h1>
         </div>
 
         <div>
@@ -37,11 +45,13 @@ async function Homepage() {
         </div>
 
         <div className="flex justify-center mt-5 sm:flex sm:justify-end sm:mr-32">
-          <button className="btn btn-info hover:bg-lime-300 font-bold text-lg">INICIAR PEDIDO</button>
+          <button className="btn btn-info hover:bg-lime-300 font-bold text-lg">
+            INICIAR PEDIDO
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
