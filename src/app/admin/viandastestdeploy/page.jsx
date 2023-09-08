9b9a28466f550e1489709d7424f1c65170dab646
davidalejoms <davidalejoms@gmail.com>
2023-09-08 16:58:42 -0500

    ' no if '

--- src/app/admin/viandastestdeploy/page.jsx
+++ src/app/admin/viandastestdeploy/page.jsx
@@ -1,15 +1,15 @@
-import { TbSquareForbid2 } from "react-icons/tb"
+// import { TbSquareForbid2 } from "react-icons/tb"
 
-import { AiFillCheckSquare } from "react-icons/ai"
+// import { AiFillCheckSquare } from "react-icons/ai"
 import Link from "next/link"
 import axios from "axios"
 import RowResponsive from "@/components/formaters/RowResponsive"
-import EditCrud from "@/components/actions/EditCrud"
-import ToogleEstadoVianda from "@/components/actions/ToogleEstadoVianda"
-import SearchBarViandas from "@/components/adminLayout/SearchBarViandas"
-import Filters from "@/components/adminLayout/Filters"
-import ClearFilters from "@/components/adminLayout/ClearFilters"
-import OrderByField from "@/components/adminLayout/OrderByField"
+// import EditCrud from "@/components/actions/EditCrud"
+// import ToogleEstadoVianda from "@/components/actions/ToogleEstadoVianda"
+// import SearchBarViandas from "@/components/adminLayout/SearchBarViandas"
+// import Filters from "@/components/adminLayout/Filters"
+// import ClearFilters from "@/components/adminLayout/ClearFilters"
+// import OrderByField from "@/components/adminLayout/OrderByField"
 
 const AdminViandasPage = async ({ searchParams }) => {
   let data = []
@@ -22,7 +22,7 @@ const AdminViandasPage = async ({ searchParams }) => {
   } catch (error) {
     data = error
   }
-  // console.log("file: page.jsx:17  data:", data)
+  console.log("file: page.jsx:17  data:", data)
 
   return (
     <div>
@@ -43,24 +43,23 @@ const AdminViandasPage = async ({ searchParams }) => {
           {/* <Filters /> */}
         </div>
         <div className="overflow-x-auto">
-          {Array.isArray(data) &&
-            data.map(({ id, imagen, nombre, tipo, descripcion, ingredientes, stock, estado }, I) => {
-              return (
-                <div
-                  className="flex flex-row"
-                  key={I}
-                >
-                  <div>{id}</div>
-                  <div>{imagen}</div>
-                  <div>{nombre}</div>
-                  <div>{tipo}</div>
-                  <div>{descripcion}</div>
-                  <div>{ingredientes}</div>
-                  <div>{stock}</div>
-                  <div>{estado}</div>
-                </div>
-              )
-            })}
+          {data.map(({ id, imagen, nombre, tipo, descripcion, ingredientes, stock, estado }, I) => {
+            return (
+              <div
+                className="flex flex-row"
+                key={I}
+              >
+                <div>{id}</div>
+                <div>{imagen}</div>
+                <div>{nombre}</div>
+                <div>{tipo}</div>
+                <div>{descripcion}</div>
+                <div>{ingredientes}</div>
+                <div>{stock}</div>
+                <div>{estado}</div>
+              </div>
+            )
+          })}
         </div>
       </RowResponsive>
     </div>
