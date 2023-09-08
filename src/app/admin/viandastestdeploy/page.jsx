3d4de26e9ed086e2e3c279e65be0ecb38ee36d63
davidalejoms <davidalejoms@gmail.com>
2023-09-08 17:03:53 -0500

    503

--- src/app/admin/viandastestdeploy/page.jsx
+++ src/app/admin/viandastestdeploy/page.jsx
@@ -42,7 +42,8 @@ const AdminViandasPage = async ({ searchParams }) => {
           </div>
           {/* <Filters /> */}
         </div>
-        <div className="overflow-x-auto">
+        <p>{data}</p>
+        {/* <div className="overflow-x-auto">
           {data.map(({ id, imagen, nombre, tipo, descripcion, ingredientes, stock, estado }, I) => {
             return (
               <div
@@ -60,7 +61,7 @@ const AdminViandasPage = async ({ searchParams }) => {
               </div>
             )
           })}
-        </div>
+        </div> */}
       </RowResponsive>
     </div>
   )
