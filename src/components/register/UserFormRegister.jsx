4eb77d7d4e25e40d5682a3b028abd8ab1b6a7acd
antonellalm <antonellarios0098@gmail.com>
2023-09-08 10:58:11 -0300

    change on register

--- src/components/register/UserFormRegister.jsx
+++ src/components/register/UserFormRegister.jsx
@@ -6,17 +6,17 @@ export const UserFormRegister = () => {
     <FormResponsiveContainer>
       <div
         id="contenedorPrincipal"
-        className=" flex flex-col items-center justify-center min-w-full"
+        className=" flex flex-col items-center justify-center min-w-full px-5 py-5"
       >
         <div id="contenedorH1" className="justify-center mb-4">
           <h1 className="text-center">CREAR CUENTA</h1>
         </div>
         <form
           action=""
-          className="min-w-full flex flex-col justify-center items-center"
+          className="min-w-full flex flex-col justify-center items-center "
         >
-          <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
-            <div className="form-control w-full  ">
+          <div className="flex flex-col md:flex-row   min-w-full gap-x-3  ">
+            <div className="form-control w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
@@ -26,80 +26,124 @@ export const UserFormRegister = () => {
                 className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
               />
             </div>
-            <div className="form-control  w-full ">
+            <div className="form-control  w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
               <input
                 type="text"
                 placeholder="Type here"
-                className="input input-bordered w-full  input-sm bg-white"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
               />
             </div>
           </div>
           <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
-            <div className="form-control w-full  ">
+            <div className="form-control w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
               <input
                 type="text"
                 placeholder="Type here"
-                className="input min-w-full input-bordered w-full  input-sm bg-white"
+                className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
               />
             </div>
-            <div className="form-control  w-full ">
+            <div className="form-control  w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
               <input
                 type="text"
                 placeholder="Type here"
-                className="input input-bordered w-full  input-sm bg-white"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
-            <div className="form-control w-full  ">
-              <label className="label">
-                <span className="label-text">What is your name?</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input min-w-full input-bordered w-full  input-sm bg-white"
-              />
-            </div>
-            <div className="form-control  w-full ">
-              <label className="label">
-                <span className="label-text">What is your name?</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full  input-sm bg-white"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
               />
             </div>
           </div>
           <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
-            <div className="form-control w-full  ">
+            <div className="form-control w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
               <input
                 type="text"
                 placeholder="Type here"
-                className="input min-w-full input-bordered w-full  input-sm bg-white"
+                className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
               />
             </div>
-            <div className="form-control  w-full ">
+            <div className="form-control  w-full pb-2 ">
               <label className="label">
                 <span className="label-text">What is your name?</span>
               </label>
               <input
                 type="text"
                 placeholder="Type here"
-                className="input input-bordered w-full  input-sm bg-white"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
+            <div className="form-control w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
+              />
+            </div>
+            <div className="form-control  w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
+            <div className="form-control w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
+              />
+            </div>
+            <div className="form-control  w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row   min-w-full gap-x-3">
+            <div className="form-control w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input min-w-full input-bordered w-full  input-sm bg-white rounded h-7"
+              />
+            </div>
+            <div className="form-control  w-full pb-2 ">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full  input-sm bg-white  rounded h-7"
               />
             </div>
           </div>
