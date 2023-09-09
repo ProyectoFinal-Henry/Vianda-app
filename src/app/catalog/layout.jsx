ddc044ff9cc71389905a59600ab8b9187fa1e153
davidalejoms <davidalejoms@gmail.com>
2023-09-09 13:44:06 -0500

    mostrar footer

--- src/app/catalog/layout.jsx
+++ src/app/catalog/layout.jsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer/Footer";
-import NavBar from "@/components/navbar/NavBar";
+import Footer from "@/components/footer/Footer"
+import NavBar from "@/components/navbar/NavBar"
 
 const layoutAdmin = ({ children }) => {
   return (
@@ -8,7 +8,7 @@ const layoutAdmin = ({ children }) => {
       {children}
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default layoutAdmin;
+export default layoutAdmin
