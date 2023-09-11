532197b812a721d000ee6566c5a75d105f5c2512
Gonodd <oddeni.gonzalo@gmail.com>
2023-09-11 16:37:43 -0300

    carritoV5

--- src/components/checkout/CardsCheckout.jsx
+++ src/components/checkout/CardsCheckout.jsx
@@ -3,21 +3,41 @@ import { currencyFormater } from "@/libs/utils/currencyFormater"
 import { useCarrito } from "@/context/CarritoContext"
 import { useEffect, useState } from "react"
 
+let id = {
+  lunes:0,
+  martes:0,
+  miercoles:0,
+  jueves:0,
+  viernes:0,
+}
+let mostrarBotonCantidad = {
+  lunes:true,
+  martes:true,
+  miercoles:true,
+  jueves:true,
+  viernes:true,
+};
+let mostrarSelectCantidad = {
+  lunes:false,
+  martes:false,
+  miercoles:false,
+  jueves:false,
+  viernes:false,
+};
 
-
-
-let id = 0;
 const CardsCheckout = async({viandasDia, dia}) => {
+  
   //Me traigo los valores guardados en localStorage por si exisitieren. Y las funciones para las modificaciones.  
-  const {viandas, agregarVianda, quitarVianda, modificarCantidad} = useCarrito();
+  const {viandas, agregarVianda, quitarVianda} = useCarrito();
   const viandaVacia = {
     id: 0,
     nombre: "Sin seleccion.",
-    tipo: "Sin seleccion.",
-    descripcion: "Sin seleccion.",
-    ingredientes: "Sin seleccion.",
+    tipo: "Sin tipo.",
+    descripcion: "Sin descricion.",
+    ingredientes: "Sin ingredientes.",
     imagen: "Sin seleccion.",
-    stock: "Sin seleccion.",
+    stock: "Sin Stock",
+    cantidad: 0,
   };
   
   //Guardo cada tipo de vianda en una variable diferente para poder manejar las posibilidades de cambio.
@@ -30,63 +50,75 @@ const CardsCheckout = async({viandasDia, dia}) => {
   //Estado para guardar las seleccion. Por default nada en caso de que no se desee pedir. O no se haya registrado
   // de manera previa.
   const [viandaSeleccionada, setViandaSeleccionada]= useState(viandaVacia);
-
-
-const index = viandas.findIndex((vianda) => vianda[dia] === true);
-const viandaGuardada = viandas[index];
+  const index = viandas.findIndex((vianda) => vianda[dia] === true);
+  const viandaGuardada = viandas[index];
 
   if (viandaSeleccionada.id === 0 && viandaGuardada){
-    id = viandaGuardada.id;
-    console.log(id)
+    id[dia] = viandaGuardada.id;
     setViandaSeleccionada(viandaGuardada);
-  }
-  if(viandaSeleccionada.id===0){
-
   }
 
   useEffect(() => {
     if (viandaSeleccionada.id !== 0) {
-      id = Number(viandaSeleccionada.id);
+      id[dia] = Number(viandaSeleccionada.id);
     }
   }, [viandaSeleccionada]);
   
-
-const handleChange = async(e)=>{
-  const seleccion = e.target.value;
-  switch (seleccion) {
-    case "clasico":
-      agregarVianda(viandaClasico);
-      setViandaSeleccionada(viandaClasico);
-      break;
+  const handleChange = async(e)=>{
+    const seleccion = e.target.value;
+    switch (seleccion) {
+      case "clasico":
+        viandaClasico.cantidad = 1;
+        agregarVianda(viandaClasico);
+        setViandaSeleccionada(viandaClasico);
+        break;
       case "sinHarina":
-      agregarVianda(viandaSinHarina);
-      setViandaSeleccionada(viandaSinHarina);
-      break;
+        viandaSinHarina.cantidad = 1;
+        agregarVianda(viandaSinHarina);
+        setViandaSeleccionada(viandaSinHarina);
+        break;
       case "vegetariano":
-      agregarVianda(viandaVegetariano);
-      setViandaSeleccionada(viandaVegetariano);
-      break;
+        viandaVegetariano.cantidad = 1;
+        agregarVianda(viandaVegetariano);
+        setViandaSeleccionada(viandaVegetariano);
+        break;
       case "dieta":
-      agregarVianda(viandaDieta);
-      setViandaSeleccionada(viandaDieta);
-      break;
+        viandaDieta.cantidad = 1;
+        agregarVianda(viandaDieta);
+        setViandaSeleccionada(viandaDieta);
+        break;
       case "viandaVacia":
-      setViandaSeleccionada(viandaVacia);
-      break;
+        break;
       default:
-      break;
+        break;
+    } 
   }
-}
 
-const handleOnClick = (e)=>{
-  quitarVianda(id)
-  setViandaSeleccionada(viandaVacia)
-}
+  const handleCambiar = (e)=>{
+    quitarVianda(id[dia])
+    setViandaSeleccionada(viandaVacia)
+  }
 
-const handleChangeCantidad =(e) => {
-  const valor = Number(e.target.value)
-  modificarCantidad(id,valor)
-}
+  const handleCantidad = (e)=>{
+    quitarVianda(id[dia])
+    mostrarSelectCantidad[dia] = true;
+    mostrarBotonCantidad[dia]= false;
+  }
+
+  const handleChangeCantidad =(e) => {
+    if(e.target.value === "borrar"){
+      quitarVianda(id[dia])
+      setViandaSeleccionada(viandaVacia)
+    }else{
+      const valor = Number(e.target.value)
+      const cambioDeCantidad = viandaSeleccionada
+      cambioDeCantidad.cantidad = valor;
+      mostrarSelectCantidad[dia] = false;
+      mostrarBotonCantidad[dia] = true;
+      setViandaSeleccionada(cambioDeCantidad);
+      agregarVianda(cambioDeCantidad);
+    }
+  }
 
   return (
     <>
@@ -97,18 +129,18 @@ const handleChangeCantidad =(e) => {
                   key={viandaSeleccionada.id}
                   id="card"
                 >
-                <select className="select select-xs select-bordered rounded w-full max-w-xs"
+                  { mostrarBotonCantidad[dia] && <button onClick={handleCantidad}>{`Cantidad: ${viandaSeleccionada.cantidad}`}</button>}
+                  { mostrarSelectCantidad[dia] && <select className="select select-xs select-bordered rounded w-full max-w-xs"
                         onChange={handleChangeCantidad}
                         >
+                          <option value="borrar">0</option>
                           <option value="1">1</option>
                           <option value="2">2</option>
                           <option value="3">3</option>
                           <option value="4">4</option>
                           <option value="5">5</option>
-                        </select>
-                  {/* <div id="cardHeader"> */}
+                        </select>}
                   <div className="avatar max-h-36">
-
                     <div
                       className="w-full
                         rounded-t-3xl"
@@ -125,14 +157,13 @@ const handleChangeCantidad =(e) => {
                   >
                     <span className="badge m-1 bg-accent/50 rounded border-none">{viandaSeleccionada.tipo}</span>
                     <h2 className="font-bold leading-4 ml-2">{viandaSeleccionada.nombre}</h2>
-                    {/* </div> */}
                     <div className="card-actions flex flex-col justify-end ">
                       <h3 className="ml-3">{currencyFormater(viandaSeleccionada.stock)}</h3>
                       <div
                         id="typeWrapper"
                         className="mx-auto mb-2"
                       >
-                      {(viandaSeleccionada.id !== 0) && <button onClick={handleOnClick}>Cambiar</button>}
+                      {(viandaSeleccionada.id !== 0) && <button onClick={handleCambiar}>Cambiar</button>}
                       {(viandaSeleccionada.id === 0)&& <select className="select select-xs select-bordered rounded w-full max-w-xs"
                         onChange={handleChange}
                         >
