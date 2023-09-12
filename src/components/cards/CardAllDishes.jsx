b7c5e274f55e98aaed7219d7b8b3b24cd27c1b4a
davidalejoms <davidalejoms@gmail.com>
2023-09-12 18:37:15 -0500

    'modal'

--- src/components/cards/CardAllDishes.jsx
+++ src/components/cards/CardAllDishes.jsx
@@ -1,10 +1,13 @@
+"use client"
 import { BsFillBoxSeamFill } from "react-icons/bs"
-import React from "react"
+import React, { useState } from "react"
 import { useRouter } from "next/navigation"
 import { Linden_Hill } from "next/font/google"
 import Link from "next/link"
+import Detalle from "../detailll/Detalle"
 
 function CardAllDishes(props) {
+  const [modal, setModal] = useState(false)
   const router = useRouter()
   const { data } = props
 
@@ -12,15 +15,31 @@ function CardAllDishes(props) {
     return <p>No hay datos disponibles</p>
   }
 
+  const modaler = () => setModal(!modal)
   return (
     <>
-      <div className="min-w-full flex flex-row flex-wrap  justify-start  my-6 gap-4">
-        {data.map(({ id, imagen, nombre, tipo, descripcion, ingredientes, stock }) => {
+      <div
+        className="min-w-full flex flex-row flex-wrap  justify-start  my-6 gap-4"
+        onClick={modaler}
+      >
+        {data.map(({ id, imagen, nombre, tipo, descripcion, ingredientes, stock, precio }) => {
           return (
             <div
               key={id}
               className="card-compact bg-amber-300 max-w-[47%]   md:max-w-[273px]  md:min-w-[273px]   shadow-xl cursor-default rounded-xl "
             >
+              {modal && (
+                <Detalle
+                  id={id}
+                  imagen={imagen}
+                  nombre={nombre}
+                  tipo={tipo}
+                  descripcion={descripcion}
+                  ingredientes={ingredientes}
+                  stock={stock}
+                  precio={precio}
+                />
+              )}
               <figure>
                 <img
                   className="rounded-t-xl object-cover min-w-full min-h-[150px] max-h-[150px] md:min-h-[200px] md:max-h-[200px]"
--- src/components/detailll/Detalle.jsx
+++ src/components/detailll/Detalle.jsx
@@ -1,90 +1,81 @@
 "use client"
-import Image from "next/image"
 
-const Detalle = () => {
+import { CgCloseO } from "react-icons/cg"
+import Image from "next/image"
+import { useState } from "react"
+
+const Detalle = ({ id, imagen, nombre, tipo, descripcion, ingredientes, stock, precio }) => {
+  const [displayed, SetDisplayed] = useState(true)
+  const toogler = () => {
+    SetDisplayed(!displayed)
+  }
+
   return (
     <>
-      <button
-        className="btn"
-        onClick={() => document.getElementById("my_modal_3").showModal()}
+      <div
+        id="modal producto"
+        className={` fixed top-0 left-0 min-h-screen min-w-full  bg-slate-900/50  flex flex-col justify-center items-center 
+        ${displayed ? "visible" : "invisible"}   `}
       >
-        open modal
-      </button>
-      <dialog
-        id="my_modal_3"
-        className="modal  "
-      >
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-            <div id="contenedorPrincipal">
-              <div className=" flex flex-col items-center justify-center place-content-center">
-                <div className="bg-accent/60 w-96 mt-4 py-1 rounded ">
-                  <h1 className="text-center font-semibold tracking-wider">Ravioles de verdura</h1>
-                </div>
-                <div
-                  id="contenedorImg"
-                  className=" w-[28rem]"
-                >
-                  <div className="avatar max-h-35 pt-4 ">
-                    <img
-                      className="object-cover"
-                      src="https://badun.nestle.es/imgserver/v1/80/1290x742/ravioli-a-la-carbonara-ligera.jpg"
-                    />
-                  </div>
-                </div>
-                <div className="avatar max-h-35 pt-4 ">
-                  <Image
-                    alt="asi funciona la app imagen"
-                    width={347}
-                    height={150}
-                    className="object-cover"
-                    src="/disponible.png"
-                  />
-                </div>
-              </div>
+        <div
+          id="contenedorPrincipal"
+          className=" max-w-[380px] min-w-[380px] flex flex-col items-center justify-center  md:max-w-[35%] md:min-w-[35%] 
+            relative bg-base-100  p-4 rounded-2xl"
+        >
+          {
+            <CgCloseO
+              className="text-accent text-2xl absolute right-2 top-2"
+              onClick={toogler}
+            />
+          }
 
-              <div
-                id="contenedorTexto"
-                className=" tracking-wider pt-5 flex flex-col gap-3 text-neutral-800 font-normal"
-              >
-                <div className="flex flex-col gap-1">
-                  <p className="bg-accent/30  pl-1 w-[6.5rem] rounded">Descripcion</p>
-                  <p className=""> Ravioles de verdura con salsa blanca</p>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <p className="bg-accent/30 w-30 pl-1 w-28 rounded">Ingredientes</p>
-                  <p className=""> Ravioles, leche, crema, maizena</p>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <p className="bg-accent/30  pl-1 w-11 rounded">Tipo</p>
-                  <p className="">Vegetariano</p>
-                </div>
-                <div className="flex flex-row gap-2">
-                  <p className="bg-accent/30 w-30 pl-1 w-14 rounded ">Stock:</p>
-                  <p className="">5</p>
-                </div>
+          <h1 className="text-center font-semibold tracking-wider text-base-100  mt-12 bg-accent min-w-[90%] mx-auto">{nombre}</h1>
 
-                <div
-                  className="divider  
-        "
-                ></div>
-                <div className="flex flex-row gap-2">
-                  <p className="bg-accent/30  pl-1 w-[3.8rem] rounded">Precio:</p>
-                  <p className="font-medium"> $ 200</p>
-                </div>
-              </div>
+          <div className="avatar min-w-full pt-4 ">
+            <img
+              src={imagen}
+              className="object-cover"
+            />
+          </div>
+          <div className="avatar max-h-35 pt-4 ">
+            <Image
+              alt="asi funciona la app imagen"
+              width={347}
+              height={150}
+              className="object-cover"
+              src="/disponible.png"
+            />
+          </div>
 
-              <div
-                id="contenedorButton  "
-                className="ml-80"
-              >
-                <button className=" py-1  btn-accent bg-accent px-8 rounded-lg mt-3 ">Agregar</button>
-              </div>
+          <div
+            id="contenedorTexto"
+            className=" tracking-wide pt-5  flex flex-col gap-2 text-neutral-800 font-normal"
+          >
+            <div className="flex flex-col gap-1">
+              <p className="bg-accent/30  pl-1 w-[6.5rem] rounded">Descripcion</p>
+              <p className=""> {descripcion}</p>
             </div>
-          </form>
+            <div className="flex flex-col gap-1">
+              <p className="bg-accent/30 w-30 pl-1 w-28 rounded">Ingredientes</p>
+              <p className="">{ingredientes}</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="bg-accent/30  pl-1 w-11 rounded">Tipo</p>
+              <p className="">{tipo}</p>
+            </div>
+            <div className="flex flex-row gap-2">
+              <p className="bg-accent/30 w-30 pl-1 w-14 rounded ">Stock:</p>
+              <p className="">{stock}</p>
+            </div>
+
+            <div className="divider"></div>
+            <div className="flex flex-row gap-2">
+              <p className="bg-accent/30  pl-1 w-[3.8rem] rounded">Precio:</p>
+              <p className="font-medium"> {precio}</p>
+            </div>
+          </div>
         </div>
-      </dialog>
+      </div>
     </>
   )
 }
