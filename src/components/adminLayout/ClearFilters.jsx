0c4e6b8fc595d80b54718242931e96ec12e59c21
FranMeza <ing.meza.francisco@gmail.com>
2023-09-12 15:58:42 -0500

    se agrego filstros a la tabla de viandas en admin

--- src/components/adminLayout/ClearFilters.jsx
+++ src/components/adminLayout/ClearFilters.jsx
@@ -1,22 +1,36 @@
 "use client";
-import Link from "next/link";
 import { LuFilterX } from "react-icons/lu";
 
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-const ClearFilters = () => {
+const ClearFilters = ({
+  setInputNombre,
+  setInputDescripcion,
+  setInputIngredientes,
+}) => {
+  const router = useRouter();
   const searchParams = useSearchParams();
 
   const search = searchParams.get("search");
+  const descripcion = searchParams.get("descripcion");
+  const ing1 = searchParams.get("ing1");
+  const ing2 = searchParams.get("ing2");
+  const ing3 = searchParams.get("ing3");
+
+  const cleanInputs = () => {
+    setInputNombre("");
+    setInputDescripcion("");
+    setInputIngredientes("");
+    router.push("/admin/viandas");
+  };
 
   return (
-    <div>
-      {search && (
-        <Link href={"/admin/viandas"}>
-          <button className=" btn btn-primary text-white ">
-            <LuFilterX className="text-2xl text-warning" />
-          </button>
-        </Link>
+    <div className="mx-auto">
+      {(search || descripcion || ing1 || ing2 || ing3) && (
+        <button className=" btn btn-warning btn-sm" onClick={cleanInputs}>
+          <LuFilterX className="text-xl text-white" />
+        </button>
       )}
     </div>
   );
---
+++ src/components/adminLayout/FiltersByField.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import ClearFilters from "./ClearFilters";
+function FiltersByField() {
+  const [inputNombre, setInputNombre] = useState("");
+  const [inputDescripcion, setInputDescripcion] = useState("");
+  const [inputIngredientes, setInputIngredientes] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "nombre") setInputNombre(value);
+    if (name === "descripcion") setInputDescripcion(value);
+    if (name === "ingredientes") setInputIngredientes(value);
+  };
+  const ingredientes = inputIngredientes.split(",");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const ing1 = ingredientes[0];
+    const ing2 = ingredientes[1];
+    const ing3 = ingredientes[2];
+
+    const params = new URLSearchParams(searchParams);
+    if (inputNombre) {
+      params.set("search", inputNombre);
+    } else {
+      params.delete("search");
+    }
+    if (inputDescripcion) {
+      params.set("descripcion", inputDescripcion);
+    } else {
+      params.delete("descripcion");
+    }
+    if (ing1 && !ing2 && !ing3) {
+      params.set("ing1", ing1);
+      params.delete("ing2");
+      params.delete("ing3");
+    } else if (ing1 && ing2 && !ing3) {
+      params.set("ing1", ing1);
+      params.set("ing2", ing2);
+      params.delete("ing3", ing3);
+    } else if (ing1 && ing2 && ing3) {
+      params.set("ing1", ing1);
+      params.set("ing2", ing2);
+      params.set("ing3", ing3);
+    } else {
+      params.delete("ing1");
+      params.delete("ing2");
+      params.delete("ing3");
+    }
+    const queryString = params.toString();
+    router.push("?" + queryString);
+  };
+
+  return (
+    <>
+      <tr>
+        <td className="b font-bold text-center">Filtros</td>
+        <td>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="border border-accent px-1 rounded-md"
+              type="text"
+              onChange={handleChange}
+              value={inputNombre}
+              name="nombre"
+            />
+          </form>
+        </td>
+        <td></td>
+        <td>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="border border-accent px-1 rounded-md"
+              type="text"
+              onChange={handleChange}
+              value={inputDescripcion}
+              name="descripcion"
+            />
+          </form>
+        </td>
+        <td>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="border border-accent px-1 rounded-md"
+              type="text"
+              onChange={handleChange}
+              value={inputIngredientes}
+              name="ingredientes"
+            />
+          </form>
+        </td>
+        <td></td>
+        <td></td>
+        <td>
+          <ClearFilters
+            setInputDescripcion={setInputDescripcion}
+            setInputIngredientes={setInputIngredientes}
+            setInputNombre={setInputNombre}
+          />
+        </td>
+      </tr>
+    </>
+  );
+}
+
+export default FiltersByField;
