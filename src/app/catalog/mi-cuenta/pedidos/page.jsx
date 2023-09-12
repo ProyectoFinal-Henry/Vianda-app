ec87ee1c05205a6edafb9b16df0eb78386c98ed0
diego34346 <diego_f423@hotmail.com>
2023-09-12 17:16:47 -0500

    cards.map pedidos ok

--- src/app/catalog/mi-cuenta/pedidos/page.jsx
+++ src/app/catalog/mi-cuenta/pedidos/page.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import MisPedidos from '@/components/mi-cuenta/MisPedidos';
+import axios from "axios";
 
-function Pedidos() {
+async function Pedidos() {
+
+  const resPedido = await axios.get(`${process.env.LOCALHOST}/api/pedidos`);
+  const dataPedido = resPedido.data[4];
+
+  const resDetalle = await axios.get(`${process.env.LOCALHOST}/api/pedidos`);
+  const dataDetalle = resDetalle.data[4]["detallePedido"];
+
   return (
     <>
-      <MisPedidos />
+      <MisPedidos dataPedido={dataPedido} dataDetalle={dataDetalle} />
     </>
   );
 }
--- src/components/mi-cuenta/MisPedidos.jsx
+++ src/components/mi-cuenta/MisPedidos.jsx
@@ -6,7 +6,8 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FiMenu } from "react-icons/fi";
 import Link from "next/link";
 
-function MisPedidos() {
+function MisPedidos({ dataDetalle, dataPedido }) {
+
   return (
     <>
       <div className="flex flex-col md:flex-row items-start">
@@ -89,17 +90,62 @@ function MisPedidos() {
           <h1 className="w-full font-bold ml-3 p-2">MIS PEDIDOS</h1>
 
           <div className="flex flex-col md:flex-row md:justify-between w-full items-center">
-
             <div className="flex flex-col md:flex-row justify-start w-full items-center ml-3">
-              <h1 className="mr-10" >Pedido No.: 877654</h1>
-              <h1 className="mr-10" >Comprado el: 23/08/2023</h1>
-              <h1 className="mr-10" >Total: $7.348</h1>
+              <h1 className="mr-10">{`Pedido No.: ${dataPedido.idTransaccion}`}</h1>
+              <h1 className="mr-10">{`Comprado el: ${dataPedido.fecha}`}</h1>
+              <h1 className="mr-10">{`Total: $${dataPedido.totalVenta}`}</h1>
             </div>
 
             <button className="flex justify-center items-center gap-x-2 first-letter:font-bold btn-accent bg-opacity-80 px-16 py-1 rounded-md w-60 mr-3  ">
               Ver pedido
             </button>
           </div>
+
+          <div className="flex flex-row justify-between items-start m-3">
+
+
+          {Array.isArray(dataDetalle) ? (
+            dataDetalle.map(({viandaId, viandaImagen, viandaNombre, precio, cantidad, total}) => {
+              return (
+                <div
+                  className="flex flex-row justify- items-center m-6 w-auto"
+                  key={viandaId}
+                >
+                  <div className="flex flex-col justify-center items-center w-auto">
+                    
+                    <div className="flex flex-col w-[50%] md:w-[100%] max-w-[250px] bg-base-100 shadow-xl my-3 border rounded-3xl border-slate900/10 md:max-w-[130px]">
+                      <div className="flex flex-col justify-start items-start">
+                        <div className="avatar max-h-36">
+                          <div
+                            className="w-full rounded-t-3xl"
+                          >
+                            <img
+                              className="object-cover"
+                              src={viandaImagen}
+                            />
+                          </div>
+                        </div>
+  
+                        <div className="flex flex-col justify-start items-start gap-1 p-1 ml-2 ">
+                          <h1 className="font-bold leading-4 my-1">
+                            {viandaNombre}
+                          </h1>
+                          <h1>Precio: {precio}</h1>
+                          <h1>Cantidad: {cantidad}</h1>
+                          <span className="badge my-2 bg-accent/50 rounded border-none">
+                            Total: {total}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            } )
+            ) : (
+              <p>No hay detalles disponibles</p>
+              )}
+              </div>
         </div>
       </div>
     </>
