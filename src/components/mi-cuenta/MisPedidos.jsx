63b7f4dd5dd6c2b736a48140ea706361a270bee2
diego34346 <diego_f423@hotmail.com>
2023-09-13 20:19:16 -0500

    Mis_pedidos-responsive

--- src/components/mi-cuenta/MisPedidos.jsx
+++ src/components/mi-cuenta/MisPedidos.jsx
@@ -7,7 +7,6 @@ import { FiMenu } from "react-icons/fi";
 import Link from "next/link";
 
 function MisPedidos({ dataDetalle, dataPedido }) {
-
   return (
     <>
       <div className="flex flex-col md:flex-row items-start">
@@ -86,66 +85,68 @@ function MisPedidos({ dataDetalle, dataPedido }) {
           </div>
         </div>
 
-        <div className="flex flex-col justify-center md:justify-start items-center bg-base-100 w-[90%] mx-[5%] mb-[5%] md:w-[300%] md:mx-[0%] md:mr-[8%] md:my-[10vh] rounded-2xl border-2 border-neutral/30 drop-shadow-lg px-2 pt-2 pb-6 ">
+        <div className="flex flex-col justify-start md:justify-start items-center bg-base-100 w-[90%] mx-[5%] mb-[5%] md:w-[300%] md:mx-[0%] mr-0 md:mr-[8%] md:my-[10vh] rounded-2xl border-2 border-neutral/30 drop-shadow-lg px-2 pt-2 pb-6 ">
           <h1 className="w-full font-bold ml-3 p-2">MIS PEDIDOS</h1>
 
           <div className="flex flex-col md:flex-row md:justify-between w-full items-center">
-            <div className="flex flex-col md:flex-row justify-start w-full items-center ml-3">
-              <h1 className="mr-10">{`Pedido No.: ${dataPedido.idTransaccion}`}</h1>
-              <h1 className="mr-10">{`Comprado el: ${dataPedido.fecha}`}</h1>
-              <h1 className="mr-10">{`Total: $${dataPedido.totalVenta}`}</h1>
+            <div className="flex flex-col md:flex-row justify-start w-full items-start ml-6 md:ml-3 mb-3 md:mb-0">
+              <h1 className="md:mr-10"><strong>Pedido No: </strong> {dataPedido.idTransaccion}</h1>
+              <h1 className="md:mr-10"><strong>Comprado el: </strong> {dataPedido.fecha}</h1>
+              <h1 className="md:mr-10"><strong>Total: </strong> ${dataPedido.totalVenta}</h1>
             </div>
 
-            <button className="flex justify-center items-center gap-x-2 first-letter:font-bold btn-accent bg-opacity-80 px-16 py-1 rounded-md w-60 mr-3  ">
+            <button className="flex justify-center items-center gap-x-2 first-letter:font-bold btn-accent bg-opacity-80 px-16 py-1 rounded w-60 md:mr-3  ">
               Ver pedido
             </button>
           </div>
 
-          <div className="flex flex-row justify-between items-start m-3">
-
-
-          {Array.isArray(dataDetalle) ? (
-            dataDetalle.map(({viandaId, viandaImagen, viandaNombre, precio, cantidad, total}) => {
-              return (
-                <div
-                  className="flex flex-row justify- items-center m-6 w-auto"
-                  key={viandaId}
-                >
-                  <div className="flex flex-col justify-center items-center w-auto">
-                    
-                    <div className="flex flex-col w-[50%] md:w-[100%] max-w-[250px] bg-base-100 shadow-xl my-3 border rounded-3xl border-slate900/10 md:max-w-[130px]">
-                      <div className="flex flex-col justify-start items-start">
-                        <div className="avatar max-h-36">
-                          <div
-                            className="w-full rounded-t-3xl"
-                          >
-                            <img
-                              className="object-cover"
-                              src={viandaImagen}
-                            />
+          <div className="flex flex-col md:flex-row md:justify-between mt-3">
+            {Array.isArray(dataDetalle) ? (
+              dataDetalle.map(
+                ({
+                  viandaId,
+                  viandaImagen,
+                  viandaNombre,
+                  precio,
+                  cantidad,
+                  total,
+                }) => {
+                  return (
+                    <div
+                      className="bg-base-100 shadow-xl border rounded-3xl border-slate900/10 my-3 md:m-4 w-80 md:w-40"
+                      key={viandaId}
+                    >
+                      
+                        <div className="flex md:flex-col flex-row justify-start items-start">
+                          <div className="avatar">
+                            <div className="w-36 max-h-48 md:w-full rounded-3xl md:rounded-b-none">
+                              <img
+                                className="object-cover"
+                                src={viandaImagen}
+                              />
+                            </div>
+                          </div>
+ 
+                          <div className="flex flex-col justify-start items-start gap-1 p-1 ml-2 mr-2 w-full">
+                            <h1 className="font-bold leading-4 my-1">
+                              {viandaNombre}
+                            </h1>
+                            <h1>Precio: ${precio}</h1>
+                            <h1>Cantidad: {cantidad}</h1>
+                            <h1 className="badge my-2 bg-accent/50 rounded border-none">
+                            <strong>Total: </strong> ${total}
+                            </h1>
                           </div>
                         </div>
-  
-                        <div className="flex flex-col justify-start items-start gap-1 p-1 ml-2 ">
-                          <h1 className="font-bold leading-4 my-1">
-                            {viandaNombre}
-                          </h1>
-                          <h1>Precio: {precio}</h1>
-                          <h1>Cantidad: {cantidad}</h1>
-                          <span className="badge my-2 bg-accent/50 rounded border-none">
-                            Total: {total}
-                          </span>
-                        </div>
-                      </div>
+                      
                     </div>
-                  </div>
-                </div>
+                  );
+                }
               )
-            } )
             ) : (
               <p>No hay detalles disponibles</p>
-              )}
-              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
