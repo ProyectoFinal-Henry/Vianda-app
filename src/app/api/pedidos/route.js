d800ae610339c528971dc24bfbdb915d4c2074fc
FranMeza <ing.meza.francisco@gmail.com>
2023-09-14 21:21:54 -0500

    tabla lista de usuarios

--- src/app/api/pedidos/route.js
+++ src/app/api/pedidos/route.js
@@ -1,27 +1,90 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
-export async function GET() {
-  try {
-    const pedidos = await prisma.pedido.findMany({
-      include: {
-        usuario: {
-          select: {
-            nombreCompleto: true,
-            email: true,
-            telefono: true,
-            direccion: true,
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+
+  if (searchParams.toString().length > 0) {
+    try {
+      const nombre = searchParams.get("nombre");
+      const estado = searchParams.get("estado");
+      const metodoPago = searchParams.get("metodo");
+      const fecha = searchParams.get("fecha");
+      const fechaFormateada = decodeURIComponent(fecha);
+
+      const whereCondicion = [];
+      if (nombre) {
+        whereCondicion.push({
+          usuario: {
+            nombreCompleto: {
+              contains: nombre,
+              mode: "insensitive",
+            },
           },
+        });
+      }
+      if (estado) {
+        whereCondicion.push({
+          estado: estado,
+        });
+      }
+
+      if (metodoPago) {
+        whereCondicion.push({
+          metodoPago: {
+            contains: metodoPago,
+            mode: "insensitive",
+          },
+        });
+      }
+      if (fecha) {
+        whereCondicion.push({
+          fecha: {
+            gte: new Date(fechaFormateada).toISOString(),
+          },
+        });
+      }
+      const pedidos = await prisma.pedido.findMany({
+        where: {
+          AND: whereCondicion,
         },
-        detallePedido: true,
-      },
-    });
-    return NextResponse.json(pedidos);
-  } catch (error) {
-    return NextResponse.json({
-      message: "Error obteniendo la lista de pedidos",
-      error: error.message,
-    });
+
+        include: {
+          usuario: true,
+        },
+      });
+      if (!pedidos || pedidos.length === 0) {
+        return NextResponse.json({
+          message: "No se encontraron pedidos con la informacion requerida",
+        });
+      }
+
+      return NextResponse.json(pedidos);
+    } catch (error) {
+      return NextResponse.json({ error: error.message });
+    }
+  } else {
+    try {
+      const pedidos = await prisma.pedido.findMany({
+        include: {
+          usuario: {
+            select: {
+              nombreCompleto: true,
+              email: true,
+              telefono: true,
+              direccion: true,
+            },
+          },
+          detallePedido: true,
+        },
+      });
+      return NextResponse.json(pedidos);
+    } catch (error) {
+      return NextResponse.json({
+        message: "Error obteniendo la lista de pedidos",
+        error: error.message,
+      });
+    }
   }
 }
 
