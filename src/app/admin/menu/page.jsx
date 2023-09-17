4b0b9a9a34dc988e1e4780df8d5a9a00e30b8a52
davidalejoms <davidalejoms@gmail.com>
2023-09-17 13:27:55 -0500

    'menu export session 2'

--- src/app/admin/menu/page.jsx
+++ src/app/admin/menu/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { GiFoodTruck } from "react-icons/gi"
 import LoadingComponentApp from "@/app/loading"
 import CardsMenu from "@/components/adminLayout/menu/CardsMenu"
 import RowResponsive from "@/components/formaters/RowResponsive"
@@ -20,15 +21,32 @@ const MenuPageAdmin = () => {
   return (
     <>
       <RowResponsive>
-        <Link href={'/admin/menu/issue-menu'} className="btn btn-primary mr-auto">EXPORTAR ESTE MENU COMO IMAGEN</Link>
         {viandas.length !== 0 ? (
           <>
+            <div className="flex flex-row justify-end flex-wrap gap-4 items-right w-full md:max-w-[896px] ">
+              <Link
+                href={"/admin/menu/issue-menu"}
+                className="btn btn-sm btn-primary rounded-md "
+              >
+                EXPORTAR ESTE MENU COMO IMAGEN
+              </Link>
+            </div>
             {semana.map((dia) => (
               <div key={dia}>
                 {dia !== "lunes" && <div className={"divider"}></div>}
-                <h1 className="text-3xl font-bold text-gray-800 block mr-auto ">
-                  Viandas para el <span className="capitalize">{dia}</span>{" "}
-                </h1>
+
+                <div className="flex flex-row justify-start items-center gap-8 ">
+                  <h1 className="text-3xl  font-bold   ">
+                    Viandas para el <span className="capitalize">{dia}</span>{" "}
+                  </h1>
+                  <Link
+                    href={`/admin/menu/issue-menu?dia=${dia}`}
+                    className="btn btn-sm btn-primary rounded-md "
+                  >
+                    exportar {dia} <GiFoodTruck className=" text-2xl" />
+                  </Link>
+                </div>
+
                 <div
                   id="menuRow"
                   className="flex flex-row justify-center flex-wrap gap-4 items-center min-w-full"
