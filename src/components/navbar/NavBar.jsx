65fd9981cb02c3df84d73841e68853ff6b364312
FranMeza <ing.meza.francisco@gmail.com>
2023-09-19 19:24:06 -0500

    correcciones en footer y dropdowns

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,52 +1,47 @@
-"use client"
-import { BiCartAdd } from "react-icons/bi"
-import { BsFillCartCheckFill } from "react-icons/bs"
-import { BsPersonCircle } from "react-icons/bs"
-
-import axios from "axios"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
-
-import Link from "next/link"
-import Image from "next/image"
-
-import { useCarrito } from "@/context/CarritoContext"
-import { UserAuth } from "@/context/AuthContext"
+"use client";
+import { BiCartAdd } from "react-icons/bi";
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { BsPersonCircle } from "react-icons/bs";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useCarrito } from "@/context/CarritoContext";
+import { UserAuth } from "@/context/AuthContext";
 
 function NavBar() {
-  const [userToken, setUserToken] = useState()
+  const [userToken, setUserToken] = useState();
+  const [logeado, setLogeado] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [carritoOpen, setCarritoOpen] = useState(false);
 
-  const { user, googleLogout } = UserAuth()
-
-  const { cantidadTotal, precioTotal } = useCarrito()
-
-  const [logeado, setLogeado] = useState(false)
-
-  const router = useRouter()
+  const { user, googleLogout } = UserAuth();
+  const { cantidadTotal, precioTotal } = useCarrito();
+  const router = useRouter();
 
   const handleGoogleLogout = async () => {
     try {
-      const response = await axios.post("/api/auth/logout")
-      if (response.status === 200) await googleLogout()
-      setLogeado(false)
-      router.push("/catalog/login")
+      const response = await axios.post("/api/auth/logout");
+      if (response.status === 200) await googleLogout();
+      setLogeado(false);
+      router.push("/catalog/login");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     axios.get("/api/auth/check").then((res) => {
       if (res.status === 200) {
-        setLogeado(true)
-        setUserToken(res.data.nombre)
+        setLogeado(true);
+        setUserToken(res.data.nombre);
       } else {
-        setLogeado(false)
-        setUserToken("")
+        setLogeado(false);
+        setUserToken("");
       }
-    })
+    });
   }),
-    [handleGoogleLogout]
+    [handleGoogleLogout];
 
   return (
     <>
@@ -62,34 +57,46 @@ function NavBar() {
         </div>
 
         <div className="flex flex-row-reverse items-center justify-between gap-x-2 ">
-          <div className="dropdown dropdown-end">
-            <label
-              tabIndex={0}
-              className="btn btn-ghost btn-circle"
-            >
+          <div
+            className="dropdown dropdown-end "
+            onClick={() => setCarritoOpen(!carritoOpen)}
+          >
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="indicator ">
-                <span className="badge badge-sm badge-primary indicator-item">{cantidadTotal}</span>
+                <span className="badge badge-sm badge-primary indicator-item">
+                  {cantidadTotal}
+                </span>
                 <BiCartAdd className="text-4xl text-warning" />
               </div>
             </label>
             <div
               tabIndex={0}
-              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+              className={`mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow ${
+                carritoOpen ? "hidden" : ""
+              }`}
             >
               <div className="card-body">
                 <span className="font-bold text-lg">{`${cantidadTotal} Items`}</span>
                 <span className="text-base ">
-                  Subtotal: <span className="font-extrabold ml-2 text-xl">${precioTotal}</span>
+                  Subtotal:{" "}
+                  <span className="font-extrabold ml-2 text-xl">
+                    ${precioTotal}
+                  </span>
                 </span>
                 <div className="card-actions">
                   <Link href={"/catalog/checkout"}>
-                    <button className="btn btn-primary btn-sm btn-block">Mi Menu Semanal</button>
+                    <button className="btn btn-primary btn-sm btn-block">
+                      Mi Menu Semanal
+                    </button>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
-          <div className="dropdown dropdown-end items-center">
+          <div
+            className="dropdown dropdown-end items-center block "
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
             <label
               tabIndex={0}
               className="btn btn-ghost btn-circle avatar mt-1"
@@ -106,13 +113,12 @@ function NavBar() {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${
+                menuOpen ? "hidden" : ""
+              }`}
             >
               <li>
-                <Link
-                  href={"/catalog/mi-cuenta"}
-                  className="justify-between"
-                >
+                <Link href={"/catalog/mi-cuenta"} className="justify-between">
                   Mi Cuenta
                 </Link>
               </li>
@@ -130,7 +136,7 @@ function NavBar() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
