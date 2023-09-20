9c7847575762fc97aaca3e2f5d86a592fa87363e
mauro fallico <mauro.fallico@Barracas.com.ar>
2023-09-20 17:13:17 -0300

    Cambio en /check a ver si el deploy mejora

--- src/app/catalog/entrega/page.jsx
+++ src/app/catalog/entrega/page.jsx
@@ -1,18 +1,18 @@
-/* import CardsPedidos from "@/components/entregaRepartidor/CardsPedidos";
+import CardsPedidos from "@/components/entregaRepartidor/CardsPedidos";
 import EstadoPedidos from "@/components/entregaRepartidor/EstadoPedidos";
 
 import axios from "axios";
 
 const EntregaPage = async () => {
-  const res = await axios.get(`${process.env.LOCALHOST}/api/pedidos`);
-  const data = res.data;
+  /* const res = await axios.get(`${process.env.LOCALHOST}/api/pedidos`);
+  const data = res.data; */
   // console.log(data);
 
   return (
     <>
-      <EstadoPedidos data={data} />
+      {/* <EstadoPedidos data={data} /> */}
     </>
   );
 };
 
-export default EntregaPage; */
+export default EntregaPage;
