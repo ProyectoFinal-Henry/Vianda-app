eb6b1516a0c037d98d20c13176f6e7dafb18dec9
mauro <maurofallico@gmail.com>
2023-09-20 20:38:35 -0300

    probando...

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -38,7 +38,7 @@ export async function POST(request) {
       const serialized = serialize("myToken", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 1000 * 3600 * 24 * 30,
         path: "/",
       });
