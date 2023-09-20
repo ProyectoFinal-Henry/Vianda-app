dd78978e7dc3608aaaca3e327135493abb4828ad
FranMeza <ing.meza.francisco@gmail.com>
2023-09-20 14:22:36 -0500

    se agregaron metadatos

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,25 @@
-import "./globals.css"
-import { Quicksand } from "next/font/google"
-import { CarritoProvider } from "../context/CarritoContext"
-import Providers from "./Providers"
-const quicksand = Quicksand({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] })
+import "./globals.css";
+import { Quicksand } from "next/font/google";
+import { CarritoProvider } from "../context/CarritoContext";
+import Providers from "./Providers";
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata = {
-  title: "ViandApp",
+  title: "ViandApp - Bienvenidos",
   description: "",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      data-theme="viandapp"
-    >
+    <html lang="en" data-theme="viandapp">
       <body className={quicksand.className}>
         <Providers>
           <CarritoProvider>{children}</CarritoProvider>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
