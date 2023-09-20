ad1201be78c46458d25bd45bd23b92e73796d5e5
davidalejoms <davidalejoms@gmail.com>
2023-09-20 10:39:28 -0500

    'small fixes'

--- src/app/repartidor/page.jsx
+++ src/app/repartidor/page.jsx
@@ -1,39 +1,114 @@
-"use client";
+"use client"
+import { useSearchParams } from "next/navigation"
 
-import React from "react";
-import { FiAward } from "react-icons/fi";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import NotAdmin from "@/components/adminLayout/NotAdmin";
+import CardsPedidos from "@/components/entregaRepartidor/CardsPedidos"
+import Modallisto from "@/components/entregaRepartidor/Modallisto"
+import React from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import NotAdmin from "@/components/adminLayout/NotAdmin"
+import Link from "next/link"
+import LoadingComponentApp from "../loading"
 
 const RepartidorDashboard = () => {
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = useState(false)
+  useEffect(() => {
+    axios.get("/api/auth/check").then((res) => {
+      if (res.data.rol !== "repartidor" && res.data.rol !== "administrador") {
+        setAuth(false)
+      } else {
+        setAuth(true)
+      }
+    })
+  }, [])
+  const [pedidos, setPedidos] = useState([])
+  const [listos, setListos] = useState([])
+  const [pendientes, setPendientes] = useState([])
+  // !=============================================
+  const searchParams = useSearchParams()
+  const params = new URLSearchParams(searchParams)
+  const queryOrden = params.get("estado-orden")
+
+  const cantidadTotal = pedidos.length
+  const cantidadListos = listos.length
+  const cantidadPendientes = pendientes.length
+
+  // !=============================================
 
   useEffect(() => {
+    estadopedido()
+  }, [])
 
-      axios.get("/api/auth/check").then((res) => {
-        if (res.data.rol !== "repartidor" && res.data.rol !== "administrador") {
-          setAuth(false);
-        } else {
-          setAuth(true);
-        }
-      });
-  }, []);
+  const estadopedido = async () => {
+    try {
+      const res = await axios.get(`/api/pedidos`) // trae todos los pedidos despachados ***HOY***
+      setPedidos(res.data)
+      const res2 = await axios.get(`/api/pedidos?estado=despachado`) // trae los pedidos con estado despachado
+      setPendientes(res2.data)
+      const res3 = await axios.get(`/api/pedidos?estado=entregado`)
+      setListos(res3.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div>
-      {auth ? ( 
+      {auth ? (
         <>
-          <div className="flex flex-col bg-base-100 items-center justify-center text-center text-4xl z-50 top-0 left-0 right-0 bottom-0 fixed">
-            <p>Bienvenido al Dashboard de Repartidor.</p>
-            <p>Aquí se visualizarán los pedidos pendientes a entregar.</p>
-            </div>
-        </>
-       ) : (
-        <NotAdmin />
-      )} 
-    </div>
-  );
-};
+          <div id="contenedor">
+            <div
+              id="contenedorEstado"
+              className="flex flex-col items-center justify-center font-semibold gap-3 bg-[#FAFF1A]  border-y-4 border-[#FF0000] pb-2"
+            >
+              <div
+                id="contenedorPedidos"
+                className="flex flex-row gap-3 h-10 items-center justify-center "
+              >
+                <div className="flex flex-row gap-1">
+                  <p className="text-2xl text-[1.625rem] ">Total: </p>
+                  <p className="text-2xl text-[1.625rem] ">{cantidadTotal}</p>
+                </div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-2xl text-[1.625rem]">Listos:</p>
+                  <p className="text-2xl text-[1.625rem] text-[#008B38]">{cantidadListos}</p>
+                </div>
+                <div className="flex flex-row gap-1">
+                  <p className="text-2xl text-[1.625rem]">Pendientes:</p>
+                  <p className="text-2xl text-[1.625rem] text-[#FF0000]">{cantidadPendientes}</p>
+                </div>
+              </div>
 
-export default RepartidorDashboard;
+              <div className=" flex flex-row gap-3 ">
+                <Link
+                  href={"/repartidor?estado-orden=entregado"}
+                  className="btn bg-[#00FF0A] text-xl font-bold px-10"
+                >
+                  <div className="flex flex-row">
+                    <p>Listos:</p>
+                    <p>{cantidadListos}</p>
+                  </div>
+                </Link>
+
+                <Link
+                  href={"/repartidor?estado-orden=despachado"}
+                  className="btn bg-[#FF0303] text-xl font-bold text-white px-5"
+                >
+                  Pendientes
+                  <p>{cantidadPendientes}</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {listos.length > 0 ? <CardsPedidos pedidos={queryOrden === "entregado" ? listos : pendientes} /> : <LoadingComponentApp />}
+
+          <Modallisto pedidos={pedidos} />
+        </>
+      ) : (
+        <NotAdmin />
+      )}
+    </div>
+  )
+}
+
+export default RepartidorDashboard
--- src/components/entregaRepartidor/CardsPedidos.jsx
+++ src/components/entregaRepartidor/CardsPedidos.jsx
@@ -1,163 +1,107 @@
-// .
-// .
-<<<<<<< HEAD
-import { FaMapMarkerAlt, FaWhatsapp, FaHome } from "react-icons/fa";
-import { AiTwotonePhone } from "react-icons/ai";
-import Modallisto from "./Modallisto";
-import Link from "next/link";
+import { FaMapMarkerAlt, FaWhatsapp, FaHome } from "react-icons/fa"
+import { AiTwotonePhone } from "react-icons/ai"
+// import Modallisto from "./Modallisto"
+import Link from "next/link"
 
 const CardsPedidos = ({ pedidos }) => {
   return (
     <>
-      <div
-        id="contenedorPrincipal"
-        className="flex flex-col gap-4 items-center justify-center font-medium mt-2 px-2 "
-      >
-=======
-const CardsPedidos = ({ pedidos, setPedidos }) => {
-  return (
-    <div
-      id="contenedorPrincipal"
-      className="flex flex-col items-center justify-center"
-    >
-      <div id="contenedorDatosPedidos">
->>>>>>> develop
-        {pedidos.map((ele) => {
-          return (
-            <div
-              id="Datos"
-              key={ele.id}
-<<<<<<< HEAD
-              className={`flex flex-col  gap-1 border-4 p-2 text-2xl font-bold    min-w-full min-h-full rounded-xl ${
-                ele.estado === "pagado"
-                  ? "bg-[#FFACAC] border-[#FF0000] "
-                  : "bg-accent border-accent"
-              } `}
-            >
-              <div className="flex flex-row gap-2 font-bold items-center pb-2 mb-3">
-                <div className="bg-white max-w-full rounded  p-2 text-2xl font-extabold">
-                  <div
-                    id="direccion "
-                    className="flex flex-col font-extabold  "
-                  >
-                    <p className="tracking-wide">Dirección:</p>
-                    <p className="font-extrabold text-3xl ">
-                      {ele.usuario.direccion}
-                    </p>
-                  </div>
-
-                  <div id="cliente" className="flex flex-row gap-1">
-                    <p className="tracking-wide">Cliente:</p>
-                    <p className="tracking-normal">
-                      {" "}
-                      {ele.usuario.nombreCompleto}
-                    </p>
-                  </div>
-                  {ele.detallePedido.map((element) => (
-                    <div key={element.pedidoId} className="flex flex-row gap-1">
-                      <p className="font-semibold">{element.cantidad}</p>
-                      <p>
-                        {element.viandaNombre.charAt(0).toUpperCase() +
-                          element.viandaNombre.slice(1)}
-                      </p>
+      {pedidos.length > 0 && (
+        <div
+          id="contenedorPrincipal"
+          className="flex flex-col gap-4 items-center justify-center font-medium mt-2 px-2 "
+        >
+          {pedidos.map((ele) => {
+            return (
+              <div
+                id="Datos"
+                key={ele.id}
+                className={`flex flex-col  gap-1 border-4 p-2 text-2xl font-bold    min-w-full min-h-full rounded-xl ${
+                  ele.estado === "despachado" ? "bg-[#FFACAC] border-[#FF0000] " : "bg-accent border-green-600"
+                } `}
+              >
+                <div className="flex flex-row gap-2 font-bold items-center pb-2 mb-3">
+                  <div className="bg-white max-w-full rounded  p-2 text-2xl font-extabold">
+                    <div
+                      id="direccion "
+                      className="flex flex-col font-extabold  "
+                    >
+                      <p className="tracking-wide">Dirección:</p>
+                      <p className="font-extrabold text-3xl ">{ele.usuario.direccion}</p>
                     </div>
-                  ))}
+
+                    <div
+                      id="cliente"
+                      className="flex flex-row gap-1"
+                    >
+                      <p className="tracking-wide">Cliente:</p>
+                      <p className="tracking-normal"> {ele.usuario.nombreCompleto}</p>
+                    </div>
+                    {/* {ele.detallePedido.map((element, i) => (
+                      
+                        <div
+                          key={i}
+                          className="flex flex-row gap-1"
+                        >
+                          <p className="font-semibold">{element.cantidad}</p>
+                          <p>{element.viandaNombre.charAt(0).toUpperCase() + element.viandaNombre.slice(1)}</p>
+                        </div>
+                      
+                    ))} */}
+                  </div>
+
+                  <div
+                    id="Pedido"
+                    className="flex flex-col items-center"
+                  >
+                    <p className="text-xl font-black">#PEDIDO:</p>
+                    <p>{ele.idTransaccion}</p>
+                  </div>
                 </div>
 
-                <div id="Pedido" className="flex flex-col items-center">
-                  <p className="text-xl font-black">#PEDIDO:</p>
-                  <p>{ele.idTransaccion}</p>
-                </div>
-              </div>
-
-              <div className="flex flex-row justify-center items-center gap-2 p-1 h-14 pb-4 mb-3">
-                <Link
-                  href={`https://api.whatsapp.com/send?phone=${ele.usuario.telefono}`}
-                >
-                  <button
-                    className="bg-white  py-2 px-3 rounded-lg transform transition-transform hover:scale-110 
+                <div className="flex flex-row justify-center items-center gap-2 p-1 h-14 pb-4 mb-3">
+                  <Link href={`https://api.whatsapp.com/send?phone=${ele.usuario.telefono}`}>
+                    <button
+                      className="bg-white  py-2 px-3 rounded-lg transform transition-transform hover:scale-110 
                   "
-                  >
-                    <FaWhatsapp className="text-6xl  text-accent" />
-                  </button>
-                </Link>
-                <Link href={`${ele.usuario.telefono}`}>
-                  {" "}
-                  <button
-                    className="bg-white  py-2 px-3 rounded-lg transform transition-transform hover:scale-110 
-                  "
-                  >
-                    <AiTwotonePhone className="text-6xl text text-slate-900" />
-                  </button>
-                </Link>
-                <Link href="https://maps.google.com">
-                  <button
-                    className="bg-white  py-2 px-3 rounded-lg transition-all duration-200 ease-in-out transform hover:scale-110 
-                  "
-                  >
-                    <FaMapMarkerAlt className="text-6xl text-red-700" />
-                  </button>
-                </Link>
-                <div className="py-2">
-                  <Link
-                    key={ele.id}
-                    href={`/admin/mensajero?modalentrega=${ele.id}`}
-                  >
-                    <button className="bg-white p-2 py-4 rounded transform transition-transform hover:scale-110 ">
-                      <FaHome
-                        className={`text-6xl bg ${
-                          ele.estado === "pagado"
-                            ? "text-warning"
-                            : "text-accent"
-                        }`}
-                      />
+                    >
+                      <FaWhatsapp className="text-6xl  text-accent" />
                     </button>
                   </Link>
-                </div>
-              </div>
-=======
-              className="flex flex-col gap-2 border border-solid p-3 mt-3 rounded border-neutral-600"
-            >
-              <div id="Pedido">
-                <p>{`PEDIDO:# ${ele.idTransaccion}`}</p>
-              </div>
-
-              <div id="direccion">
-                <p>Dirección:</p>
-                <p>{ele.usuario.direccion}</p>
-              </div>
-
-              <div id="cliente">
-                <p>Cliente:</p>
-                <p> {ele.usuario.nombreCompleto}</p>
-              </div>
-
-              {ele.detallePedido.map((element) => (
-                <div key={element.pedidoId}>
-                  <span>Vianda:</span>
-                  <p>{element.viandaNombre}</p>
-                  <div className="flex flex-row gap-1">
-                    <span>Cantidad de viandas :</span>
-                    <p>{element.cantidad}</p>
+                  <Link href={`${ele.usuario.telefono}`}>
+                    {" "}
+                    <button
+                      className="bg-white  py-2 px-3 rounded-lg transform transition-transform hover:scale-110 
+                  "
+                    >
+                      <AiTwotonePhone className="text-6xl text text-slate-900" />
+                    </button>
+                  </Link>
+                  <Link href="https://maps.google.com">
+                    <button
+                      className="bg-white  py-2 px-3 rounded-lg transition-all duration-200 ease-in-out transform hover:scale-110 
+                  "
+                    >
+                      <FaMapMarkerAlt className="text-6xl text-red-700" />
+                    </button>
+                  </Link>
+                  <div className="py-2">
+                    <Link
+                      key={ele.id}
+                      href={`/admin/entrega?modalentrega=${ele.id}`}
+                    >
+                      <button className="bg-white p-2 py-4 rounded transform transition-transform hover:scale-110 ">
+                        <FaHome className={`text-6xl bg ${ele.estado === "pagado" ? "text-warning" : "text-accent"}`} />
+                      </button>
+                    </Link>
                   </div>
                 </div>
-              ))}
->>>>>>> develop
-            </div>
-          );
-        })}
-      </div>
-<<<<<<< HEAD
+              </div>
+            )
+          })}
+        </div>
+      )}
     </>
-=======
-      <div id="contenedorBotones">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
-    </div>
->>>>>>> develop
-  );
-};
-export default CardsPedidos;
+  )
+}
+export default CardsPedidos
