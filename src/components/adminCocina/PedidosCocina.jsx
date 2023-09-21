6a23f04b215632ae3e49fa22e244bf6c82018a6f
lugatti <lugatti92@gmail.com>
2023-09-20 23:17:46 -0300

    actualizacion que cambia el estado de pagado a despachado

--- src/components/adminCocina/PedidosCocina.jsx
+++ src/components/adminCocina/PedidosCocina.jsx
@@ -2,28 +2,32 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const PedidosCocina = ({ dataPedido }) => {
   const fechaHoy = new Date().toLocaleDateString();
   const router = useRouter();
-  const [filtro, setFiltro] = useState("todos"); // en principio muestra todos los pedidos
-  const [detallesEstado, setDetallesEstado] = useState({}); // Estado para rastrear el estado de cada detalle del pedido, los uso en los colores de las viandas
+  const [filtro, setFiltro] = useState("todos");
+  const [detallesEstado, setDetallesEstado] = useState({});
   const [pedidoSeleccionado, setPedidoSeleccionado] = useState(null);
+  const [estadisticas, setEstadisticas] = useState({
+    totalPedidos: 0,
+    pedidosPagados: 0,
+    pedidosCocinados: 0,
+  });
 
-  
   useEffect(() => {
     const { estado } = router.query || {};
     if (estado) {
       setFiltro(estado);
     }
-}, [router.query]);
+  }, [router.query]);
 
-//! Función para actualizar el estado y agregar el parámetro "estado" a la URL
-const filtrarPorEstado = (estado) => {
-  setFiltro(estado);
-  router.push(`/admin/cocina?estado=${estado}`); // Actualiza la URL con el parámetro "estado"
-};
-  //! Filtra los pedidos según el estado seleccionado
+  const filtrarPorEstado = (estado) => {
+    setFiltro(estado);
+    router.push(`/admin/cocina?estado=${estado}`);
+  };
+
   const pedidosFiltrados =
     filtro === "pagados"
       ? dataPedido.filter((pedido) => pedido.estado === "pagado")
@@ -31,13 +35,12 @@ const filtrarPorEstado = (estado) => {
       ? dataPedido.filter((pedido) => pedido.estado === "despachado")
       : dataPedido;
 
-  //! Función para cambiar el estado de un detalle del pedido
   useEffect(() => {
     const inicializarEstadoDetalles = () => {
       const estadoInicial = {};
       dataPedido.forEach((pedido) => {
         pedido.detallePedido.forEach((detalle) => {
-          const key = `${pedido.idTransaccion}-${detalle.viandaId}`;
+          const key = `${pedido.id}-${detalle.viandaId}`;
           estadoInicial[key] = "pendiente";
         });
       });
@@ -47,7 +50,6 @@ const filtrarPorEstado = (estado) => {
     inicializarEstadoDetalles();
   }, [dataPedido]);
 
-  //! Función para cambiar el estado de las viandas del pedido
   const cambiarEstadoDetalle = (pedidoId, viandaId) => {
     setDetallesEstado((prevState) => {
       const key = `${pedidoId}-${viandaId}`;
@@ -66,38 +68,34 @@ const filtrarPorEstado = (estado) => {
       return nuevoEstado;
     });
 
-    const pedido = dataPedido.find((pedido) => pedido.idTransaccion === pedidoId);
+    const pedido = dataPedido.find((pedido) => pedido.id === pedidoId);
     if (pedido) {
       const viandasEstado = pedido.detallePedido.map(
         (detalle) => detallesEstado[`${pedidoId}-${detalle.viandaId}`]
       );
-      if (!viandasEstado.includes("pendiente") && !viandasEstado.includes("en proceso")) {
+      if (
+        !viandasEstado.includes("pendiente") &&
+        !viandasEstado.includes("en proceso")
+      ) {
         setPedidoSeleccionado(pedido);
       }
     }
   };
 
-  const despacharPedido = () => {
-    if (pedidoSeleccionado) {
-      pedidoSeleccionado.estado = "despachado";
-      setPedidoSeleccionado(null);
-    }
-
-  };
-
-  //! Función para verificar el estado del pedido
   const verificarEstadoPedido = (pedidoId) => {
-    const pedido = dataPedido.find(
-      (pedido) => pedido.idTransaccion === pedidoId
-    );
+    const pedido = dataPedido.find((pedido) => pedido.id === pedidoId);
 
     if (!pedido) {
-      return "cocinado"; // Si no se encuentra el pedido, se considera "cocinado" por defecto
+      return "cocinado";
     }
 
     const viandasEstado = pedido.detallePedido.map((detalle) => {
       return detallesEstado[`${pedidoId}-${detalle.viandaId}`];
     });
+
+    if (filtro === "despachado") {
+      return "cocinado";
+    }
 
     if (viandasEstado.includes("pendiente")) {
       return "pendiente";
@@ -107,16 +105,42 @@ const filtrarPorEstado = (estado) => {
       return "cocinado";
     }
   };
+  const despacharPedido = async (idPedido, estado) => {
+    const resultado = await axios.put(`/api/pedidos/`, { idPedido, estado });
+  };
 
+  const calcularEstadisticas = () => {
+    const totalPedidos = dataPedido.length;
+    const pedidosPagados = dataPedido.filter(
+      (pedido) => pedido.estado === "pagado"
+    ).length;
+    const pedidosCocinados = dataPedido.filter(
+      (pedido) => pedido.estado === "despachado"
+    ).length;
 
-
-  console.log(pedidosFiltrados);
+    setEstadisticas({
+      totalPedidos,
+      pedidosPagados,
+      pedidosCocinados,
+    });
+  };
+  useEffect(() => {
+    calcularEstadisticas();
+  }, [filtro, dataPedido]);
 
   return (
     <>
       <div className="flex justify-between items-center mx-10">
         <h1 className="font-bold ml-3 p-2">PEDIDOS POR ENTREGAR</h1>
         <h1 className="flex items-end"> {fechaHoy} </h1>
+      </div>
+
+      <div className="flex justify-start bg-yellow-300 border-2 border-red-500">
+        <p className="mr-7">Total de Pedidos: {estadisticas.totalPedidos}</p>
+        <p className="mr-7">Pedidos Pagados: {estadisticas.pedidosPagados}</p>
+        <p className="mr-7">
+          Pedidos Cocinados/Despachados: {estadisticas.pedidosCocinados}
+        </p>
       </div>
 
       <div className="flex justify-center mt-3">
@@ -151,17 +175,19 @@ const filtrarPorEstado = (estado) => {
           {pedidosFiltrados.map((pedido) => (
             <div
               className={`flex flex-col justify-center md:justify-center items-center ${
-                verificarEstadoPedido(pedido.idTransaccion) === "pendiente"
+                filtro === "despachado"
+                  ? "bg-green-200"
+                  : verificarEstadoPedido(pedido.id) === "pendiente"
                   ? "bg-red-200"
-                  : verificarEstadoPedido(pedido.idTransaccion) === "en proceso"
+                  : verificarEstadoPedido(pedido.id) === "en proceso"
                   ? "bg-yellow-200"
                   : "bg-green-200"
               } w-[100%] mx-5 my-5 rounded-2xl border-2 border-neutral/30 drop-shadow-lg px-2 pt-2`}
-              key={pedido.idTransaccion}
+              key={pedido.id}
             >
               <div className="flex justify-around w-full items-start ml-6 md:ml-3 mb-4 md:mb-0 p-4 rounded-lg">
                 <h1 className="md:mr-10">
-                  <strong>Pedido No: </strong> {pedido.idTransaccion}
+                  <strong>Pedido No: </strong> {pedido.id}
                 </h1>
                 <h1 className="md:mr-10">
                   <strong>Para: </strong>
@@ -173,34 +199,45 @@ const filtrarPorEstado = (estado) => {
                 <h1 className="md:mr-10">
                   <strong>Total: </strong> ${pedido.totalVenta}
                 </h1>
-<button
-          className={`font-bold px-3 py-1 rounded md:mr-3 border-2 border-neutral/30 drop-shadow-lg ${
-            pedidoSeleccionado ? "bg-green-500 text-white" : verificarEstadoPedido(pedido.idTransaccion) === "pendiente"
-              ? "bg-red-500 text-white"
-              : verificarEstadoPedido(pedido.idTransaccion) === "en proceso"
-              ? "bg-yellow-500 text-white"
-              : "bg-green-500 text-white"
-          }`}
-          onClick={() => {
-            if (pedidoSeleccionado) {
-              despacharPedido();
-            } else if (verificarEstadoPedido(pedido.idTransaccion) === "cocinado") {
-              if (window.confirm("¿Deseas despachar este pedido?")) {
-                despacharPedido();
-              }
-            }
-          }}
-        >
-          Pedido {pedidoSeleccionado ? "Despachado" : verificarEstadoPedido(pedido.idTransaccion) === "pendiente" ? "Pendiente" : verificarEstadoPedido(pedido.idTransaccion) === "en proceso" ? "En Proceso" : "Cocinado"}
-        </button>
+                <button
+                  className={`font-bold px-3 py-1 rounded md:mr-3 border-2 border-neutral/30 drop-shadow-lg ${
+                    filtro === "despachado" &&
+                    verificarEstadoPedido(pedido.id) === "cocinado"
+                      ? "bg-green-500 text-white"
+                      : filtro !== "despachado" &&
+                        verificarEstadoPedido(pedido.id) === "cocinado"
+                      ? "bg-green-500 text-white"
+                      : "bg-gray-300 text-gray-600 cursor-not-allowed"
+                  }`}
+                  onClick={() => {
+                    const idPedido = pedido.id;
+                    const estado = "despachado";
+
+                    if (
+                      verificarEstadoPedido(idPedido) === "cocinado" &&
+                      window.confirm("¿Deseas despachar este pedido?")
+                    ) {
+                      despacharPedido(idPedido, estado);
+                    }
+                  }}
+                >
+                  Pedido{" "}
+                  {verificarEstadoPedido(pedido.id) === "cocinado"
+                    ? "Despachado"
+                    : verificarEstadoPedido(pedido.id) === "pendiente"
+                    ? "Pendiente"
+                    : verificarEstadoPedido(pedido.id) === "en proceso"
+                    ? "En Proceso"
+                    : "Cocinado"}
+                </button>
               </div>
               <div className="flex flex-col md:flex-row md:justify-around mt-5 px-2">
                 {pedido.detallePedido.map((detalle, index) => (
                   <div
-                    className=" shadow-xl border rounded-3xl border-slate900/10 my-6  w-10 md:w-48 bg-zinc-50"
+                    className="flex mr-4 shadow-xl border rounded-3xl border-slate900/10 my-6  w-10 md:w-48 bg-zinc-50"
                     key={index}
                   >
-                    <div className="flex md:flex-col flex-row justify-center items-center">
+                    <div className="flex md:flex-col flex-row items-center">
                       <div className="avatar">
                         <div className="w-36 max-h-48 md:w-full md:rounded-b-none rounded-xl">
                           <img
@@ -210,7 +247,7 @@ const filtrarPorEstado = (estado) => {
                         </div>
                       </div>
 
-                      <div className="flex flex-col justify-center items-center gap-1 p-1 ml-2 mr-2 w-full">
+                      <div className="flex flex-col justify-between items-center gap-1 p-1 ml-2 mr-2 w-full">
                         <h1 className="font-bold leading-4 my-1">
                           {" "}
                           {detalle.viandaNombre}{" "}
@@ -218,28 +255,27 @@ const filtrarPorEstado = (estado) => {
                         <h1>Cantidad: {detalle.cantidad}</h1>
                         <button
                           className={`flex justify-center items-center px-6 py-5 font-bold rounded m-auto mt-5 border-2 border-neutral/30 drop-shadow-lg ${
-                            detallesEstado[
-                              `${pedido.idTransaccion}-${detalle.viandaId}`
-                            ] === "pendiente"
+                            filtro === "despachado"
+                              ? "bg-green-500 text-white"
+                              : detallesEstado[
+                                  `${pedido.id}-${detalle.viandaId}`
+                                ] === "pendiente"
                               ? "bg-red-500 text-white"
                               : detallesEstado[
-                                  `${pedido.idTransaccion}-${detalle.viandaId}`
+                                  `${pedido.id}-${detalle.viandaId}`
                                 ] === "en proceso"
                               ? "bg-yellow-500 text-white"
                               : "bg-green-500 text-white"
                           }`}
                           onClick={() =>
-                            cambiarEstadoDetalle(
-                              pedido.idTransaccion,
-                              detalle.viandaId
-                            )
+                            cambiarEstadoDetalle(pedido.id, detalle.viandaId)
                           }
                         >
-                          {
-                            detallesEstado[
-                              `${pedido.idTransaccion}-${detalle.viandaId}`
-                            ]
-                          }
+                          {filtro === "despachado"
+                            ? "Cocinado"
+                            : detallesEstado[
+                                `${pedido.id}-${detalle.viandaId}`
+                              ]}
                         </button>
                       </div>
                     </div>
