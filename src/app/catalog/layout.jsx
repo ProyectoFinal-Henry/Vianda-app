92deb11e0e6924ae91ed5e7347551d7a118bb980
mauro <maurofallico@gmail.com>
2023-09-22 00:44:40 -0300

    probando cositas

--- src/app/catalog/layout.jsx
+++ src/app/catalog/layout.jsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/navbar/NavBar";
+import { cookies } from 'next/headers'
+import jwt from 'jsonwebtoken'
 
 export const metadata = {
   title: "ViandApp - Comida Saludable",
@@ -7,9 +9,12 @@ export const metadata = {
 };
 
 const layoutAdmin = ({ children }) => {
+  const cookieStore = cookies()
+  const token = cookieStore.get('myToken')
+  const tokenData = jwt.verify(token.value, 'secret')  
   return (
     <div>
-      <NavBar />
+      <NavBar tokenData={tokenData}/>
       {children}
       <Footer />
     </div>
