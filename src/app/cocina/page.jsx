15e2994cf7272ef1b409cb97f1033b623361d1ee
lugatti <lugatti92@gmail.com>
2023-09-22 19:55:38 -0300

    mas actualizaciones

--- src/app/cocina/page.jsx
+++ src/app/cocina/page.jsx
@@ -1,39 +1,18 @@
-"use client";
-
-import React from "react";
-import { FiAward } from "react-icons/fi";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import NotAdmin from "@/components/adminLayout/NotAdmin";
+import PedidosCocina from "@/components/adminCocina/PedidosCocina";
 
-const CocinaDashboard = () => {
-  const [auth, setAuth] = useState(false);
+const AdminCocina = async () => {
 
-  useEffect(() => {
-
-      axios.get("/api/auth/check").then((res) => {
-        if (res.data.rol !== "cocina" && res.data.rol !== "administrador") {
-          setAuth(false);
-        } else {
-          setAuth(true);
-        }
-      });
-  }, []);
+    const resPedido = await axios.get(`${process.env.LOCALHOST}/api/pedidos`);
+    const dataPedido = resPedido.data;
 
   return (
-    <div>
-       {auth ? ( 
-        <>
-          <div className="flex flex-col bg-base-100 items-center justify-center text-center text-4xl z-50 top-0 left-0 right-0 bottom-0 fixed">
-            <p>Bienvenido al Dashboard de Cocina.</p>
-            <p>Aquí se visualizarán los pedidos pendientes a preparar.</p>
-            </div>
-        </>
-       ) : (
-        <NotAdmin />
-      )} 
-    </div>
+    <>
+      <PedidosCocina dataPedido={dataPedido}  />
+    </>
   );
 };
 
-export default CocinaDashboard;
+export default AdminCocina;
+
+
