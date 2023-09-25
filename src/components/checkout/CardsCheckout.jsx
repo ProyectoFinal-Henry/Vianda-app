c8bb23525012d6f9f89ba5cbffafa7f602733ad0
davidalejoms <davidalejoms@gmail.com>
2023-09-24 19:28:26 -0500

    'last'

--- src/components/checkout/CardsCheckout.jsx
+++ src/components/checkout/CardsCheckout.jsx
@@ -159,7 +159,7 @@ const CardsCheckout = ({ viandasDia, dia }) => {
               <Image
               width={200}
               height={200}
-              className="rounded-t-xl object-cover min-w-full min-h-[180px] max-h-[180px] md:min-h-[120px] md:max-h-[120px] w-auto h-auto"
+              className="rounded-t-xl object-cover min-h-[180px] max-h-[180px] md:min-h-[120px] md:max-h-[120px]  min-w-full"
               src={viandaSeleccionada.imagen}
                 alt={viandaSeleccionada.nombre}
               />
