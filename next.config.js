1dc862c72ff15475eb7657151d307a8e25a21c4d
ProyectoFinal-Henry <143524928+ProyectoFinal-Henry@users.noreply.github.com>
2024-09-16 10:38:55 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*", // This applies the header to all routes
-        headers: [
+    images: {
+        remotePatterns: [
           {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups", // Adjusting COOP to allow popups
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp", // Optional for added security
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/**',
           },
         ],
       },
-    ];
-  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
